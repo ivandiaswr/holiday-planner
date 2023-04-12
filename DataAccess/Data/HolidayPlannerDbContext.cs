2e89a4a9affd614c5146a36e12bc912a407ba56d
Ivan Dias <ivandiasworkrelated@gmail.com>
2023-04-12 15:02:39 +0100

    removed plans seed

--- DataAccess/Data/HolidayPlannerDbContext.cs
+++ DataAccess/Data/HolidayPlannerDbContext.cs
@@ -10,22 +10,22 @@ namespace DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            List<Plan> plansToSeed = new List<Plan>();
+            //List<Plan> plansToSeed = new List<Plan>();
 
-            for (int i = 1; i <= 6; i++)
-            {
-                plansToSeed.Add(new Plan
-                {
-                    Id = i,
-                    Name = "Name" + i.ToString(),
-                    Place = "Place" + i.ToString(),
-                    StartDate = DateTime.Now,
-                    EndDate = DateTime.Now,
-                    AddicionalInformation = "",
-                });
-            }
+            //for (int i = 1; i <= 6; i++)
+            //{
+            //    plansToSeed.Add(new Plan
+            //    {
+            //        Id = i,
+            //        Name = "Name" + i.ToString(),
+            //        Place = "Place" + i.ToString(),
+            //        StartDate = DateTime.Now,
+            //        EndDate = DateTime.Now,
+            //        AddicionalInformation = "",
+            //    });
+            //}
 
-            modelBuilder.Entity<Plan>().HasData(plansToSeed);
+            //modelBuilder.Entity<Plan>().HasData(plansToSeed);
         }
     }
 }
